97f4584c3eda2db77f86b7a078d7671a2824cb5f
Gabriel Brizolla Dorneles <gabrielbrizolladorneles@gmail.com>
2026-09-08 15:43:45 -0300

    Adiciona cadastro e listagem de produtos

--- main.py
+++ main.py
@@ -1,3 +1,49 @@
+produtos = []
+
+def cadastrar_produto():
+    print("\n--- CADASTRO DE PRODUTO ---")
+
+    codigo = len(produtos) + 1
+
+    categoria = input("Categoria: ")
+    marca = input("Marca: ")
+    modelo = input("Modelo: ")
+    cor = input("Cor: ")
+    quantidade = int(input("Quantidade: "))
+    preco = float(input("Preço: R$ "))
+
+    produto = {
+        "codigo": codigo,
+        "categoria": categoria,
+        "marca": marca,
+        "modelo": modelo,
+        "cor": cor,
+        "quantidade": quantidade,
+        "preco": preco
+    }
+
+    produtos.append(produto)
+
+    print("\nProduto cadastrado com sucesso!")
+    print(f"Código do Produto: {codigo}")
+
+def listar_produtos():
+    print("\n--- LISTA DE PRODUTOS ---")
+
+    if len(produtos) == 0:
+        print("Nenhum produto cadastrado.")
+        return
+
+    for produto in produtos:
+        print("\n----------------------------")
+        print(f"Código: {produto['codigo']}")
+        print(f"Categoria: {produto['categoria']}")
+        print(f"Marca: {produto['marca']}")
+        print(f"Modelo: {produto['modelo']}")
+        print(f"Cor: {produto['cor']}")
+        print(f"Quantidade: {produto['quantidade']}")
+        print(f"Preço: R$ {produto['preco']:.2f}")
+
 while True:
     print("\n==============================")
     print("      CONTROLE DE ESTOQUE")
@@ -15,7 +61,13 @@ while True:
     
     opcao = input("\nEscolha uma opção: ")
 
-    if opcao == "0":
+    if opcao == "1":
+        cadastrar_produto()
+
+    elif opcao == "2":
+        listar_produtos()
+
+    elif opcao == "0":
         print("\n Sistema encerrado.")
         break
     else:
