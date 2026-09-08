0248cc2c6e44287de9e540d4247169f795bf070a
Gabriel Brizolla Dorneles <gabrielbrizolladorneles@gmail.com>
2026-09-08 14:38:37 -0300

    Cria menu principal do sistema

---
+++ main.py
@@ -0,0 +1,22 @@
+while True:
+    print("\n==============================")
+    print("      CONTROLE DE ESTOQUE")
+    print("==============================")
+
+    print("1 - Cadastrar produto")
+    print("2 Listar produtos")
+    print("3 - Buscar produto")
+    print("4 - Entrada de estoque")
+    print("5 - Saída de estoque")
+    print("6 - Editar produto")
+    print("7 - Excluir produto")
+    print("8 - Relatório do estoque")
+    print("0 - Sair")
+    
+    opcao = input("\nEscolha uma opção: ")
+
+    if opcao == "0":
+        print("\n Sistema encerrado.")
+        break
+    else:
+        print("\nOpção ainda não implementada.")
